fe54acb668959a4a9245e5c7da1877acc68f6ef1
Aidan Murray <aidanhansm@gmail.com>
2020-12-14 09:23:05 -0700

    Start spec for user story

---
+++ spec/features/food_view_spec.rb
@@ -0,0 +1,16 @@
+describe 'As a user,' do
+  describe 'When I visit "/" And I fill in the search form with "sweet potatoes"' do
+    it 'And I click "Search"' do
+      visit '/'
+      # search for foods
+      # Then I should be on page "/foods"
+      # Then I should see a total of the number of items returned by the search.
+      # Then I should see a list of ten foods that contain the ingredient "sweet potatoes"
+      # And for each of the foods I should see:
+      # - The food's GTIN/UPC code
+      # - The food's description
+      # - The food's Brand Owner
+      # - The food's ingredients
+    end
+  end
+end
