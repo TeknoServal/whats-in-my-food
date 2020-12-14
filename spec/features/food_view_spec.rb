b2f5b3fd40e28225d39a0da645f43cbc20e785c1
Aidan Murray <aidanhansm@gmail.com>
2020-12-14 10:09:22 -0700

    Finish user spec story for food show

--- spec/features/food_view_spec.rb
+++ spec/features/food_view_spec.rb
@@ -2,15 +2,27 @@ describe 'As a user,' do
   describe 'When I visit "/" And I fill in the search form with "sweet potatoes"' do
     it 'And I click "Search"' do
       visit '/'
-      # search for foods
-      # Then I should be on page "/foods"
-      # Then I should see a total of the number of items returned by the search.
-      # Then I should see a list of ten foods that contain the ingredient "sweet potatoes"
+
+      fill_in(:q, with: 'apple')
+
+      expect(page).to have_current_path('/foods')
+
+      expect(page).to have_content('39244 results')
+
+      expect(page).to have_css('.food-block', count: 10)
+
       # And for each of the foods I should see:
-      # - The food's GTIN/UPC code
-      # - The food's description
-      # - The food's Brand Owner
-      # - The food's ingredients
+      page.all('.food-block').each do |food_block|
+        # - The food's GTIN/UPC code
+        expect(food_block).to have_css('.food-code')
+        # - The food's description
+        expect(food_block).to have_css('.food-description')
+        # - The food's Brand Owner
+        expect(food_block).to have_css('.food-brand')
+        # - The food's ingredients
+        expect(food_block).to have_css('.food-ingredients')
+
+      end
     end
   end
 end
