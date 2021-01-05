1b2adbccbbd67de173b2fef4ebfc227410a1957e
Aidan Murray <aidanhansm@gmail.com>
2021-01-05 11:01:04 -0700

    Add caching for results of food api

--- app/facades/food_search_facade.rb
+++ app/facades/food_search_facade.rb
@@ -1,12 +1,14 @@
 class FoodSearchFacade
   def self.search_food(query)
-    parsed_response = FDAFoodService.search_food(query)
-    # require 'pry'; binding.pry
-    foods = instantiate_foods(parsed_response[:foods])
-    {
-      result_count: parsed_response[:totalHits],
-      foods: foods
-    }
+    Rails.cache.fetch("food_query/#{query}", expires_in: 7.days) do
+      parsed_response = FDAFoodService.search_food(query)
+      # require 'pry'; binding.pry
+      foods = instantiate_foods(parsed_response[:foods])
+      {
+        result_count: parsed_response[:totalHits],
+        foods: foods
+      }
+    end
   end
 
   def self.instantiate_foods(foods)
